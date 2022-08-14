cfd18aa8860923e90cc37380bee0d5bdaeb18a0b
Emmanuel Aaron Popa Campusano <emmanuelaaronpopa@gmail.com>
2022-08-14 19:54:02 -0400

    Pull De pal de cosas del front

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,7 @@
-// const express = require("express");
+//const express = require("express");
 const trailer = require("../components/trailer/view");
 const user = require("../components/user/view");
+
 
 const routes = function (server) {
   server.use("/trailer", trailer);
